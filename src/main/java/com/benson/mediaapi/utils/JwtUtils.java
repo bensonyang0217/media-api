c3c73caf10416a8515799c7e4ba7ecc6cba5675a
Benson Yang <f932037@gmail.com>
2024-02-15 14:49:32 +0800

    update

--- src/main/java/com/benson/mediaapi/utils/JwtUtils.java
+++ src/main/java/com/benson/mediaapi/utils/JwtUtils.java
@@ -3,8 +3,10 @@ package com.benson.mediaapi.utils;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.slf4j.LoggerFactory;
 
 import java.security.Key;
 import java.util.Base64;
@@ -13,6 +15,8 @@ import java.util.function.Function;
 
 @Component
 public class JwtUtils {
+    private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
+
     private static final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS512;
 
     private static final String BASE64_SECRET_KEY = "0c8bac35ebeee7085106b6cd95a113b90640521adb9297bc407274923917658cf70a1c568a93981ac401055aca4b82fc463c48d7ac6ae28e8c191f617d65ff0a";
@@ -27,9 +31,11 @@ public class JwtUtils {
         return Keys.hmacShaKeyFor(decodedKey);
     }
 
+    private static final Key key = generateKey();
+
     public static String generateToken(String username) {
         return Jwts.builder().subject(username).issuedAt(new Date()).expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(generateKey(), signatureAlgorithm)
+                .signWith(key, signatureAlgorithm)
                 .compact();
     }
 
@@ -59,10 +65,11 @@ public class JwtUtils {
 
     public static boolean validateToken(String token) {
         try {
-            Jws<Claims> claims = Jwts.parser().setSigningKey(generateKey()).build().parseClaimsJws(token);
+            Jws<Claims> claims = Jwts.parser().setSigningKey(key).build().parseClaimsJws(token);
             return !claims.getBody().getExpiration().before(new Date());
         } catch (JwtException | IllegalArgumentException e) {
             // Log the error or handle it as per your application's requirement
+            logger.error(e.getMessage());
             return false;
         }
     }
