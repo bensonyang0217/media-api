146539b036426caaf822abf9b232b5871fd90b05
Benson Yang <f932037@gmail.com>
2024-02-15 09:41:52 +0800

    update model

---
+++ src/main/java/com/benson/mediaapi/model/ImageInfo.java
@@ -0,0 +1,76 @@
+package com.benson.mediaapi.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class ImageInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String fileName;
+    private long fileSize;
+    private LocalDateTime uploadDate;
+    private boolean thumbnailStatus;
+    private String thumbnailUrl;
+    private Long userId;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public long getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(long fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public LocalDateTime getUploadDate() {
+        return uploadDate;
+    }
+
+    public void setUploadDate(LocalDateTime uploadDate) {
+        this.uploadDate = uploadDate;
+    }
+
+    public boolean isThumbnailStatus() {
+        return thumbnailStatus;
+    }
+
+    public void setThumbnailStatus(boolean thumbnailStatus) {
+        this.thumbnailStatus = thumbnailStatus;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
